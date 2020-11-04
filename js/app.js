ffb9a8e8fc7feffdd4c983b6af8d868357445bbc
mirko1987 <47920868+mirko1987@users.noreply.github.com>
2020-11-04 22:30:51 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var app = angular.module('universidadApp',['ngRoute','ui.mask']);
+
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+  
+	$scope.menuSuperior = 'parciales/menu.html';
+	$scope.telefonoMask = "9999-9999";
+
+
+	$scope.setActive = function(Opcion){
+
+		$scope.mInicio     = "";
+		$scope.mProfesores = "";
+		$scope.mAlumnos    = "";
+
+		$scope[Opcion] = "active";
+
+	}
+
+}]);
+
+// Filtro personalizado de teléfono
+app.filter('telefono',function(){
+
+	return function(numero){
+		return numero.substring(0,4) + "-"+numero.substring(4);
+	}
+
+});
+
